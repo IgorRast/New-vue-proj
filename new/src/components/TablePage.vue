fa7ce7513a916a4bb72e4767529020d231de0439
Игорь Растрыгин <jyoung@yandex.ru>
2022-10-31 12:34:14 +0300

    docs:create vue proj

---
+++ new/src/components/TablePage.vue
@@ -0,0 +1,60 @@
+<template>
+  <div>
+  <v-data-table
+    :headers="headers"
+    :items="row"
+    hide-default-footer
+    class="elevation-1"
+    disable-pagination
+    
+    >
+    
+  </v-data-table>
+  <v-btn depressed v-on:click="addRow">
+    Add Row
+  </v-btn>
+ 
+  
+</div>
+</template>
+
+<script>
+
+export default {
+data () {
+  return {
+    headers: [
+      {
+        text: 'Date',
+        align: 'start',
+        sortable: false,
+        value: 'date',
+      },
+      { text: 'Number', value: 'number' },
+      { text: 'Value', value: 'value' },
+     
+    ],
+    row: [
+     
+    ],
+    
+  }
+  
+},
+methods: {
+  addRow() {
+    let time = new Date()
+    this.row.push( {
+        date: time.getHours() + ':' + time.getMinutes(),
+        number: parseInt(Math.random() * (100 - (-100)) + (-100)),
+        value: '1'
+      }
+    )
+  },
+ 
+ 
+},
+
+
+}
+</script>
