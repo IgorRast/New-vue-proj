c3062d7469595d3aa5d93f53bdfda22e2361d6a7
Игорь Растрыгин <jyoung@yandex.ru>
2022-11-04 20:31:57 +0300

    edit: sort fucntion

--- new/src/components/TablePage.vue
+++ new/src/components/TablePage.vue
@@ -1,8 +1,58 @@
 <template>
   <div>
-  <v-data-table
+    <v-toolbar
+  dense
+  elevation="4"
+  flat
+>
+<v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+<v-toolbar-title>Menu</v-toolbar-title>
+</v-toolbar>
+<v-navigation-drawer
+      app
+      left
+      absolute
+      temporary
+      v-model="drawer"
+    >
+    <v-list-item-group>
+        
+        <v-list-item >
+          <v-list-item-title  @click="changeTableA">Show Table A</v-list-item-title>
+        </v-list-item>
+        <v-list-item >
+          <v-list-item-title @click="changeTableB">Show Table B</v-list-item-title>
+        </v-list-item>  
+        <v-list-item >
+          <v-list-item-title @click="changeTableC">Show Table C </v-list-item-title>
+        </v-list-item>
+        
+      </v-list-item-group>
+      
+  </v-navigation-drawer>
+  <v-data-table v-if="showTableA"
     :headers="headers"
     :items="row"
+    hide-default-footer
+    class="elevation-1"
+    disable-pagination
+    
+    >
+    
+  </v-data-table>
+  <v-data-table v-else-if="showTableB"
+    :headers="headersB"
+    :items="rowB"
+    hide-default-footer
+    class="elevation-1"
+    disable-pagination
+    
+    >
+    
+  </v-data-table>
+  <v-data-table v-else-if="showTableC"
+    :headers="headersC"
+    :items="rowC"
     hide-default-footer
     class="elevation-1"
     disable-pagination
@@ -23,6 +73,9 @@
 export default {
 data () {
   return {
+    showTableA: true,
+    showTableB: false,
+    showTableC: false,
     headers: [
       {
         text: 'Date',
@@ -37,6 +90,36 @@ data () {
     row: [
      
     ],
+    headersB: [
+      {
+        text: 'B',
+        align: 'start',
+        sortable: false,
+        value: 'date',
+      },
+      { text: 'B', value: 'number' },
+      { text: 'B', value: 'value' },
+     
+    ],
+    rowB: [
+     
+    ],
+    headersC: [
+      {
+        text: 'C',
+        align: 'start',
+        sortable: false,
+        value: 'date',
+      },
+      { text: 'C', value: 'number' },
+      { text: 'C', value: 'value' },
+     
+    ],
+    rowC: [
+     
+    ],
+    drawer: false,
+
     
   }
   
@@ -44,46 +127,48 @@ data () {
 methods: {
   addRow() {
     let time = new Date()
-    const holidays = [  'Mon Jan 03 2022 00:00:00 GMT+0300 (Москва, стандартное время)',
-                        'Tue Jan 04 2022 00:00:00 GMT+0300 (Москва, стандартное время)',
-                        'Wed Jan 05 2022 00:00:00 GMT+0300 (Москва, стандартное время)',
-                        'Thu Jan 06 2022 00:00:00 GMT+0300 (Москва, стандартное время)',
-                        'Fri Jan 07 2022 00:00:00 GMT+0300 (Москва, стандартное время)',
-                        'Sat Jan 02 2022 00:00:00 GMT+0300 (Москва, стандартное время)',
-                        'Wed Feb 23 2022 00:00:00 GMT+0300 (Москва, стандартное время)',
-                        'Mon Mar 07 2022 00:00:00 GMT+0300 (Москва, стандартное время)',
-                        'tue Mar 08 2022 00:00:00 GMT+0300 (Москва, стандартное время)',
-                        'Mon May 02 2022 00:00:00 GMT+0300 (Москва, стандартное время)',
-                        'Tue May 03 2022 00:00:00 GMT+0300 (Москва, стандартное время)',
-                        'Mon May 09 2022 00:00:00 GMT+0300 (Москва, стандартное время)',
-                        'Tue May 10 2022 00:00:00 GMT+0300 (Москва, стандартное время)'
+    const holidays = [  new Date('Mon Jan 03 2022 00:00:00 GMT+0300 (Москва, стандартное время)'),
+        new Date('Tue Jan 04 2022 00:00:00 GMT+0300 (Москва, стандартное время)'),
+        new Date(  'Wed Jan 05 2022 00:00:00 GMT+0300 (Москва, стандартное время)'),
+                        // 'Thu Jan 06 2022 00:00:00 GMT+0300 (Москва, стандартное время)',
+                        // 'Fri Jan 07 2022 00:00:00 GMT+0300 (Москва, стандартное время)',
+                        // 'Sat Jan 02 2022 00:00:00 GMT+0300 (Москва, стандартное время)',
+                        // 'Wed Feb 23 2022 00:00:00 GMT+0300 (Москва, стандартное время)',
+                        // 'Mon Mar 07 2022 00:00:00 GMT+0300 (Москва, стандартное время)',
+                        // 'tue Mar 08 2022 00:00:00 GMT+0300 (Москва, стандартное время)',
+                        // 'Mon May 02 2022 00:00:00 GMT+0300 (Москва, стандартное время)',
+                        // 'Tue May 03 2022 00:00:00 GMT+0300 (Москва, стандартное время)',
+                        // 'Mon May 09 2022 00:00:00 GMT+0300 (Москва, стандартное время)',
+                        // 'Tue May 10 2022 00:00:00 GMT+0300 (Москва, стандартное время)'
 
                       ]
     const getBusinessDays = (startDate, endDate) => {
     const start = new Date(startDate);
-    console.log(start)
+    
     const end = new Date(endDate);
-    const current = start;
+    
     const dates = [];
     
     //const dateFree = dates.filter(x => !holidays.includes(x));
-
-    while (current <= end) {
-      if (current.getDay() !== 6 && current.getDay() !== 0) {
-        dates.push(new Date(current));
+    //holidays.indexOf(current) > -1
+    while (start <= end) {
+      console.log(new Date(start))
+      const match = holidays.find(d => d.getTime() === start.getTime())
+      const hasMatch = !!match;
+      if (start.getDay() !== 6 && start.getDay() !== 0 && !hasMatch) {
+        dates.push(new Date(start));
       }
 
-      current.setDate(current.getDate() + 1);
+      start.setDate(start.getDate() + 1);
     }
 
-    return dates.filter(function(start){
-        return holidays.indexOf(start) < 0;
-    });
+    return dates
+    
     
   }
 
  
-const businessDays = getBusinessDays('01-01-2022', '11-31-2022');  
+const businessDays = getBusinessDays('01-01-2022', '01-10-2022');  
     this.row.push( {
         date: time.getHours() + ':' + time.getMinutes(),
         number: parseInt(Math.random() * (100 - (-100)) + (-100)),
@@ -91,7 +176,27 @@ const businessDays = getBusinessDays('01-01-2022', '11-31-2022');
       }
     )
   },
- 
+changeTableB() { 
+  
+  this.showTableB = true
+  this.showTableA = false
+  this.showTableC = false
+  
+  
+},
+changeTableA() { 
+  this.showTableB = false
+  this.showTableA = true
+  this.showTableC = false
+  
+},
+changeTableC() { 
+  this.showTableB = false
+  this.showTableA = false
+  this.showTableC = true
+},
+
+
  
 },
 
